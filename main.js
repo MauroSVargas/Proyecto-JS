13d048eda6943e0c18643798c893ee8b3b420e19
mauro_vargas <mauroseba2000@gmail.com>
2022-09-26 23:03:13 -0300

    primer pre entrega proyecto JS

---
+++ main.js
@@ -0,0 +1,94 @@
+let Saldo= 5000
+let Entrada1= 2500
+let Entrada2= 5000
+let Entrada3= 7500
+let Entrada4= 10000
+
+
+//--------------------------------------funciones que suceden una vez elijo entrada---------------------------------//
+
+function IngreseTarjeta(tarjeta){     
+    if (tarjeta=== "0011"){
+        let clave = prompt("ingrese su clave")
+        IngreseClave(clave)
+    } else {
+        alert("numero de tarjeta incorrecto")
+    }
+}
+
+function IngreseClave(clave) {
+    if (clave=== "1234"){ 
+    }
+    else {
+        alert("clave de tarjeta incorrecto")
+    }
+}
+
+function MontoTicket1(){
+   if (Saldo >= Entrada1){
+        alert("pago realizado con exito!")
+    } else{
+        alert("saldo insuficiente")
+    }
+}
+
+function MontoTicket2(){
+    if (Saldo >= Entrada2){
+         alert("pago realizado con exito!")
+     } else{
+         alert("saldo insuficiente")
+     }
+}
+
+function MontoTicket3(){
+    if (Saldo >= Entrada3){
+         alert("pago realizado con exito!")
+     } else{
+         alert("saldo insuficiente")
+     }
+}
+
+function MontoTicket4(){
+    if (Saldo >= Entrada4){
+         alert("pago realizado con exito!")
+     } else{
+         alert("saldo insuficiente")
+     }
+}
+
+
+
+//--------------------------------------funciones a la hora de elegir entrada---------------------------------//
+
+function PagoTicket1(){
+    let tarjeta = prompt("ingrese su tarjeta")
+    IngreseTarjeta(tarjeta)
+    let clave = prompt("ingrese su clave")
+    IngreseClave(clave)
+    MontoTicket1()
+}
+
+function PagoTicket2(){
+    let tarjeta = prompt("ingrese su tarjeta")
+    IngreseTarjeta(tarjeta)
+    let clave = prompt("ingrese su clave")
+    IngreseClave(clave)
+    MontoTicket2()
+}
+
+function PagoTicket3(){
+    let tarjeta = prompt("ingrese su tarjeta")
+    IngreseTarjeta(tarjeta)
+    let clave = prompt("ingrese su clave")
+    IngreseClave(clave)
+    MontoTicket3()
+}
+
+function PagoTicket4(){
+    let tarjeta = prompt("ingrese su tarjeta")
+    IngreseTarjeta(tarjeta)
+    let clave = prompt("ingrese su clave")
+    IngreseClave(clave)
+    MontoTicket4()
+}
+
